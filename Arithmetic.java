98d6c013eb0f687ee1c3ac3b70a12baaa5186731
Raunak Mandal <iamriju2000@gmail.com>
2021-10-07 00:13:00 +0530

    fix

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,33 +1,19 @@
 import java.util.Scanner;
 
 class Arithmetic {
-	double no1, no2;
-	char ch;
-	void set_values(double x, double y, char which){
-		no1 = x;
-		no2 = y;
-		ch = which;
-	}
-
-	double getresult() {
-		switch (ch) {
-			case '+':
-				return no1 + no2;
-			case '-':
-				return no1 - no2;
-			case '*':
-				return no1 * no2;
-			case '/':
-			 	return no1 / no2;
-			default: return 0;
-		}
-	}
+	double add(double no1, double no2) {return no1 + no2;}
+	double subtract(double no1, double no2){return no1 - no2;};
+	double multiply(double no1, double no2){return no1 * no2;};
+	double divide(double no1, double no2){return no1 / no2;};
 }	
 
 class Maths {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+
+		int ch = 1;
 		
+		do {
 		System.out.print("Enter operand 1: ");
 		double num1 = sc.nextDouble();
 		System.out.print("Enter operand 2: ");
@@ -36,7 +22,28 @@ class Maths {
 		char op = sc.next().charAt(0);
 
 		Arithmetic arith = new Arithmetic();
-		arith.set_values(num1, num2, op);
-		System.out.println("Arithmatic Result: " + arith.getresult());
+
+		System.out.print("Arithmatic Result: ");
+		double res = 0.0;
+
+		switch (op) {
+			case '+':
+				res = arith.add(num1, num2);
+				break;
+			case '-':
+				res = arith.subtract(num1, num2);
+				break;
+			case '*':
+				res = arith.multiply(num1, num2);
+				break;
+			case '/':
+				res = arith.divide(num1, num2);
+				break;	
+			default: break;
+		}
+		System.out.println(res);
+		System.out.print("Press any number to continue and 0 to exit: ");
+		ch = sc.nextInt();
+		} while(ch != 0);
 	}
 }
