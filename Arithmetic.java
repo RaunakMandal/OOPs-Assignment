162bec0e1db2eed81e93060408942aebb113f925
Raunak Mandal <iamriju2000@gmail.com>
2021-10-06 23:46:51 +0530

    changes

--- Arithmetic.java
+++ Arithmetic.java
@@ -1,25 +1,23 @@
+import java.util.Scanner;
+
 class Arithmetic {
 	double no1, no2;
-	int ch;
-	void set_values(double x, double y, int which){
+	char ch;
+	void set_values(double x, double y, char which){
 		no1 = x;
 		no2 = y;
 		ch = which;
 	}
-	// double add() {return no1 + no2;}
-	// double subs() {return no1 - no2;}
-	// double mul() {return no1 * no2;}
-	// double div() {return no1 / no2;}
 
 	double getresult() {
 		switch (ch) {
-			case 1:
+			case '+':
 				return no1 + no2;
-			case 2:
+			case '-':
 				return no1 - no2;
-			case 3:
+			case '*':
 				return no1 * no2;
-			case 4:
+			case '/':
 			 	return no1 / no2;
 			default: return 0;
 		}
@@ -28,8 +26,17 @@ class Arithmetic {
 
 class Maths {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter operand 1: ");
+		double num1 = sc.nextDouble();
+		System.out.print("Enter operand 2: ");
+		double num2 = sc.nextDouble();
+		System.out.print("Enter operator (+, -, *, /): ");
+		char op = sc.next().charAt(0);
+
 		Arithmetic arith = new Arithmetic();
-		arith.set_values(Integer.parseInt(args[0]), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+		arith.set_values(num1, num2, op);
 		System.out.println("Arithmatic Result: " + arith.getresult());
 	}
 }
--- room.java
+++ room.java
@@ -1,24 +1,34 @@
-class Room
-{
-	double height,length,breadth;
-	void set_values(double h,double l,double b)
-	{
-		height = h;
+import java.util.Scanner;
+
+class Room {
+	double length, breadth, height;
+
+	void set_values(double l, double b, double h) {
 		length = l;
 		breadth = b;
+		height = h;
 	}
-	double volume()
-	{
-		return length*breadth*height;
+
+	double getVolume() {
+		return length * breadth * height;
 	}
 }
 
-class Room_demo
-{
-	public static void main(String args[])
-	{
-		Room room1 = new Room();
-		room1.set_values(10,10,10);
-		System.out.println("Volume is: "+room1.volume());	
+class Room_demo {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter height: ");
+		double height = sc.nextDouble();
+		System.out.print("Enter breadth: ");
+		double breadth = sc.nextDouble();
+		System.out.print("Enter length: ");
+		double length = sc.nextDouble();
+
+		System.out.println("Height: " + height + " Breadth: " + breadth + " Length: " + length);
+
+		Room room = new Room();
+		room.set_values(length, breadth, height);
+
+		System.out.println("Area of the room: " + room.getVolume());
 	}
 }
