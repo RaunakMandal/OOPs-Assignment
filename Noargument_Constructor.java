1481e822599eca4a1d72eff5643bd4130ca138ad
Debadyuti Karmakar <62183757+DEBADYUTI-KARMAKAR@users.noreply.github.com>
2021-10-06 22:36:06 +0530

    Noargument_Constructor

---
+++ Noargument_Constructor.java
@@ -0,0 +1,23 @@
+//No Argument
+
+class A{
+	
+	
+	public A(){
+		System.out.println("Hello Java");
+	}
+}
+class Noargument_Constructor{
+	public Noargument_Constructor(){
+		System.out.println("HII");
+	}
+
+	public static void main(String[] args) {
+
+
+
+		A print = new A();
+		Noargument_Constructor n = new Noargument_Constructor();
+
+	}
+}
